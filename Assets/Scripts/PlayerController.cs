5acc2a02b1f8858afc8c372961392302b33a83fe
Noah Robichaux <34008564@mansd.org>
2023-09-25 14:05:45 -0400

    9/25/23 class progress

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D playerRigidbody2D;
+
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            playerRigidbody2D.constraints = RigidbodyConstraints2D.None;
+            playerRigidbody2D.constraints = RigidbodyConstraints2D.FreezePositionX;
+            playerRigidbody2D.constraints = RigidbodyConstraints2D.FreezeRotation;
+            playerRigidbody2D.AddForce(new Vector2(0, speed), ForceMode2D.Force);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerRigidbody2D.constraints = RigidbodyConstraints2D.None;
+            playerRigidbody2D.constraints = RigidbodyConstraints2D.FreezePositionX;
+            playerRigidbody2D.constraints = RigidbodyConstraints2D.FreezeRotation;
+            playerRigidbody2D.AddForce(new Vector2(0, -speed), ForceMode2D.Force);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            playerRigidbody2D.constraints = RigidbodyConstraints2D.FreezeAll;
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            playerRigidbody2D.constraints = RigidbodyConstraints2D.FreezeAll;
+        }
+    }
+
+}
