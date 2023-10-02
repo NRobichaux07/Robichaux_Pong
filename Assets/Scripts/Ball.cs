a4da5b1913e36868d326bce9f186a0adbffd57bf
Noah Robichaux <34008564@mansd.org>
2023-10-02 14:08:54 -0400

    class progress 10/2/2023

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,17 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Ball : MonoBehaviour
 {
+    public GameObject puck;
     public Rigidbody2D puckRB;
-    public Vector2 puckDirection;
+    private Vector2 puckDirection;
     public GameObject labelTextP1;
     public GameObject labelTextP2;
+    public bool isPuckGoingRight;
+    public TMP_Text p1ScoreText;
+    public TMP_Text p2ScoreText;
+    public float playTime;
+    public int p1Score;
+    public int p2Score;
+    public GameObject p1Bar;
+    public GameObject p2Bar;
+    private Vector2 p1InitPosition;
+    private Vector2 p2InitPosition;
+
+
     // Start is called before the first frame update
     void Start()
     {
         puckRB = GetComponent<Rigidbody2D>();
+        p1InitPosition = p1Bar.transform.position;
+        p2InitPosition = p2Bar.transform.position;
+        isPuckGoingRight = true;
+        playTime += Time.realtimeSinceStartup;
+        p1Score = 0;
+        p2Score = 0;
     }
 
     // Update is called once per frame
@@ -21,10 +41,16 @@ public class Ball : MonoBehaviour
         {
             labelTextP1.SetActive(false);
             labelTextP2.SetActive(false);
+            puckRB.constraints = RigidbodyConstraints2D.None;
+            puckRB.constraints = RigidbodyConstraints2D.FreezeRotation;
             float x = Random.Range(0f, 10f);
             float y = 10f;
             puckDirection = new Vector2(x, y);
             puckRB.AddForce(puckDirection, ForceMode2D.Force);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && playTime > 0)
+        {
+            return;
         }
     }
 
@@ -32,18 +58,38 @@ public class Ball : MonoBehaviour
     {
         if (collision.gameObject.tag == "WallTop")
         {
-            float x = Random.Range(0f, 10f);
-            float y = -10f;
-            puckDirection = new Vector2(x, y);
-            puckRB.AddForce(puckDirection, ForceMode2D.Force);
+            if (isPuckGoingRight == true)
+            {
+                float x = Random.Range(0f, 10f);
+                float y = -10f;
+                puckDirection = new Vector2(x, y);
+                puckRB.AddForce(puckDirection, ForceMode2D.Force);
+            }
+            if (isPuckGoingRight == false)
+            {
+                float x = Random.Range(-10f, 0f);
+                float y = -10f;
+                puckDirection = new Vector2(x, y);
+                puckRB.AddForce(puckDirection, ForceMode2D.Force);
+            }
         }
 
         if (collision.gameObject.tag == "WallBottom")
         {
-            float x = Random.Range(-10f, 0f);
-            float y = 10f;
-            puckDirection = new Vector2(x, y);
-            puckRB.AddForce(puckDirection, ForceMode2D.Force);
+            if (isPuckGoingRight == true)
+            {
+                float x = Random.Range(0f, 10f);
+                float y = 10f;
+                puckDirection = new Vector2(x, y);
+                puckRB.AddForce(puckDirection, ForceMode2D.Force);
+            }
+            if (isPuckGoingRight == false)
+            {
+                float x = Random.Range(-10f, 0f);
+                float y = 10f;
+                puckDirection = new Vector2(x, y);
+                puckRB.AddForce(puckDirection, ForceMode2D.Force);
+            }
         }
 
         if (collision.gameObject.tag == "Player1")
@@ -52,6 +98,7 @@ public class Ball : MonoBehaviour
             float y = Random.Range(0f, 10f);
             puckDirection = new Vector2(x, y);
             puckRB.AddForce(puckDirection, ForceMode2D.Force);
+            isPuckGoingRight = true;
         }
 
         if (collision.gameObject.tag == "Player2")
@@ -60,6 +107,34 @@ public class Ball : MonoBehaviour
             float y = Random.Range(-10f, 0f);
             puckDirection = new Vector2(x, y);
             puckRB.AddForce(puckDirection, ForceMode2D.Force);
+            isPuckGoingRight = false;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "ScoreLineLeft")
+        {
+            p2Score += 1;
+            p2ScoreText.SetText("" + p2Score);
+            puck.transform.position = new Vector2(6.7f, 0f);
+            puckRB.constraints = RigidbodyConstraints2D.FreezeAll;
+            labelTextP1.SetActive(true);
+            labelTextP2.SetActive(true);
+            isPuckGoingRight = false;
+            p1Bar.transform.position = p1InitPosition;
+            p2Bar.transform.position = p2InitPosition;
+        }
+        if (collision.gameObject.tag == "ScoreLineRight")
+        {
+            p1Score += 1;
+            p1ScoreText.SetText("" + p1Score);
+            puck.transform.position = new Vector2(-6.7f, 0f);
+            puckRB.constraints = RigidbodyConstraints2D.FreezeAll;
+            labelTextP1.SetActive(true);
+            labelTextP2.SetActive(true);
+            isPuckGoingRight = true;
+            p1Bar.transform.position = p1InitPosition;
+            p2Bar.transform.position = p2InitPosition;
         }
     }
 }
